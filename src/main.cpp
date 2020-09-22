da94040bde3f71845a7e6665327f5d3174ab5ea1
sammoth <sammoth.th@gmail.com>
2020-09-22 17:58:54 +0100

    version bump and about box license info

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include "stdafx.h"
 // PCH ^
 
-DECLARE_COMPONENT_VERSION("Pulseaudio Output", "0.5-beta",
-                          "Pulseaudio network output");
+const char* about = "Copyright (c) sammoth, 2020\n"
+"\n"
+"This component uses PulseAudio, licensed under the LGPL v2.1, see:\n"
+"https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html\n"
+"\n"
+"Source code is provided at:\n"
+"https://github.com/sammoth/foo_out_pulse/releases";
+
+DECLARE_COMPONENT_VERSION("Pulseaudio Output", "0.6-beta",
+                          about);
 VALIDATE_COMPONENT_FILENAME("foo_out_pulse.dll");
